d144a8eb5f34e071f09407809ae7ce476de5ed53
RezaRostaminikoo <RezaRostaminikoo@gmail.com>
2023-11-03 01:03:17 -0400

    feat:add optional to literal definition

--- src/TypeLiteralGenerator.ts
+++ src/TypeLiteralGenerator.ts
@@ -1,5 +1,6 @@
-import ts from 'typescript'
+import ts, { QuestionToken, isQuestionToken } from 'typescript'
 import { Writer } from './writer'
+import { ModifierHandler, ModifierLikeHandler } from './modifiers'
 
 /** this class is to create literal type
  * @example
@@ -19,14 +20,23 @@ export class TypeLiteralGenerator {
      * @param {ts.TypeNode} type 
      * @returns {this}
      */
-    addProperty(name: string, type: ts.TypeNode): this {
+    addProperty(name: string, type: ts.TypeNode, optional: boolean = false): this {
         if (!name) throw Error("TypeLiteralGenerator.addProperty | the name should be defined")
         if (!type) throw Error("TypeLiteralGenerator.addProperty | the type should be defined")
-        this._property_signatures.push(ts.factory.createPropertySignature(undefined, name, undefined, type))
+
+        this._property_signatures.push(
+            ts.factory.createPropertySignature(
+                undefined,
+                name,
+                optional ? ts.factory.createToken(ts.SyntaxKind.QuestionToken) : undefined,
+                type
+            )
+        )
         return this
     }
 
     generate(): ts.TypeLiteralNode {
+
         return ts.factory.createTypeLiteralNode(this._property_signatures)
     }
 
